ba920c4e477aee4790ddf7d5b6b3c8ee02be7ceb
kelvinhovinho <kelvinadamba@hotmail.com>
2020-12-14 18:26:41 +0300

    add validate function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","S
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-// function validate() {
+function validate() {
 //   var genders = document.getElementsByName("gender");
 //   if( document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value >2020 || document.myForm.year.value <=1900) {
 //      alert( "Please provide a valid year of birth! eg 2019" );
