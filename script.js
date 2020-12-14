c6f7957402505135437e22ee6ba14a9b963e58b2
kelvinhovinho <kelvinadamba@hotmail.com>
2020-12-14 18:33:24 +0300

    add var genders

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 function validate() {
-//   var genders = document.getElementsByName("gender");
-//   if( document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value >2020 || document.myForm.year.value <=1900) {
-//      alert( "Please provide a valid year of birth! eg 2019" );
-//      document.myForm.year.focus() ;
-//      return false;
-//   }
+  var genders = document.getElementsByName("gender");
+  if( document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value >2020 || document.myForm.year.value <=1900) {
+     alert( "Please provide a valid year of birth! eg 2019" );
+     document.myForm.year.focus() ;
+     return false;
+  }
 //   else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
 //   document.myForm.month.value.length != 1 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
 //      alert( "Please provide your month of birth! between 1 and 12" );
@@ -30,7 +30,7 @@ function validate() {
 //     return true ;
 //   }
   
-// }
+}
 
 // function calculateDayValue(){
 //   year = document.getElementById("year").value;
